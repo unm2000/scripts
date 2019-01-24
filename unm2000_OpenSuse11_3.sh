3a061cf25b238eb69a5d2a10bdde30aa9e2fd0c6
unm2000 <46225827+unm2000@users.noreply.github.com>
2019-01-24 11:19:54 -0200

    Add files via upload

---
+++ unm2000_OpenSuse11_3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+versao=$(cat /etc/*-release | awk '{print tolower($0)}')
+
+if [[ $versao != *"opensuse 11.3"* ]]; then
+	echo -e "Sistema Operacional incompativel!\nCompativel somenete com: OpenSuse 11.3"
+	exit
+fi
+
+echo "Baixando arquivos de instalação..."
+wget -q --no-check-certificate https://raw.githubusercontent.com/unm2000/scripts/master/gdown.pl -O gdown.pl && chmod u+x gdown.pl
+
+perl ./gdown.pl 'https://docs.google.com/uc?export=download&id=1kRPrAExcEZVYTK31PUsRIXxm_a12oqfv' unm2000_20171122_OpenSuse11_3.run
+
+chmod +x unm2000_20171122_OpenSuse11_3.run 
+sh unm2000_20171122_OpenSuse11_3.run
